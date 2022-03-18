43f1f888055df8f1e4baef63f64881aa0d048bb0
josephguluzada <yquluzade98@gmail.com>
2022-03-18 18:06:48 +0400

    Mini Console App Example

---
+++ WeaponTask/WeaponTask/Models/Weapon.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeaponTask.Models
+{
+    public class Weapon
+    {
+        private int _bulletCapacity;
+
+        public int BulletCapacity { 
+            get 
+            {
+                return _bulletCapacity;
+            } 
+            set 
+            { 
+                if(value > 0 && value <= 120)
+                {
+                    _bulletCapacity = value;
+                }
+            } 
+        }
+        public int CurrentBulletCount { get; set; }
+        public double DisChargeTime { get; set; }
+        public bool FireMode { get; set; }
+
+        public Weapon()
+        {
+
+        }
+
+        public Weapon(int bulletCapacity,int bulletCount)
+        {
+            this.BulletCapacity = bulletCapacity;
+            this.CurrentBulletCount = bulletCapacity;
+        }
+
+        // Shoot Method
+
+        public void Shoot()
+        {
+            if(CurrentBulletCount > 0)
+            {
+                --CurrentBulletCount;
+                Console.WriteLine("1 bullet shot!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have any bullet in your magazine.");
+            }
+        }
+
+        // Fire Method
+
+        public void Fire()
+        {
+            if (CurrentBulletCount > 0)
+            {
+                if (FireMode == true)
+                {
+                    double fireTime = (CurrentBulletCount * DisChargeTime) / BulletCapacity; 
+                    CurrentBulletCount = 0;
+
+                    Console.WriteLine(Math.Round(fireTime,2) + " sec");
+                }
+                else
+                {
+                    --CurrentBulletCount;
+                    Console.WriteLine("1 bullet shot");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You don't have any bullet in your magazine.");
+            }
+        }
+
+        // Get Remain Bullet Count Method
+
+        public int GetRemainBulletCount()
+        {
+            return BulletCapacity - CurrentBulletCount;
+        }
+
+        // Reload Method
+
+        public void Reload()
+        {
+            if(BulletCapacity != CurrentBulletCount)
+            {
+                int neededBullets = GetRemainBulletCount();
+                CurrentBulletCount += neededBullets;
+
+                Console.WriteLine($"Reloaded! Added {neededBullets} bullets to magazine.");
+            }
+            else
+            {
+                Console.WriteLine("You don't need reload.");
+            }
+        }
+
+
+        // Change Fire Mode
+
+        public void ChangeFireMode()
+        {
+            if (FireMode == true)
+            {
+                FireMode = false;
+                Console.WriteLine("Single mode activated");
+            }
+            else
+            {
+                FireMode = true;
+                Console.WriteLine("Auto mode activated");
+
+            }
+        }
+
+        // Change Bullet Capacity
+        public void ChangeBulletCapacity(int newBulletCapacity)
+        {
+            this.BulletCapacity = newBulletCapacity;
+            this.CurrentBulletCount = _bulletCapacity;
+            Console.WriteLine($"Your new bullet capacity is {_bulletCapacity}");
+        }
+    }
+}
---
+++ WeaponTask/WeaponTask/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using WeaponTask.Models;
+
+namespace WeaponTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bulletCapacity;
+
+            do
+            {
+                Console.WriteLine("Enter the maximum bullet capacity:");
+                bulletCapacity = Convert.ToInt32(Console.ReadLine());
+
+            } while (bulletCapacity<=0 || bulletCapacity>120);
+
+            int bulletCount;
+
+            do
+            {
+                Console.WriteLine("Enter the bullets:");
+                bulletCount = Convert.ToInt32(Console.ReadLine());
+
+            } while (bulletCount <=0 || bulletCount > bulletCapacity);
+
+            Weapon weapon = new Weapon(bulletCapacity,bulletCount);
+
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Choose the command (0: Info)");
+
+                /*Another variant of menu:*/
+
+                //Console.WriteLine("0: Info\n" +
+                //    "1: Shoot\n" +
+                //    "2: Fire\n" +
+                //    "3: Remain Bullet Count\n" +
+                //    "4: Reload\n" +
+                //    "5: Change Fire Mode\n" +
+                //    "6: Exit");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("------------------------");
+                        Console.WriteLine(
+                    "1: Shoot\n" +
+                    "2: Fire\n" +
+                    "3: Remain Bullet Count\n" +
+                    "4: Reload\n" +
+                    "5: Change Fire Mode\n" +
+                    "6: Exit\n" +
+                    "7: Edit");
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 1:
+                        Console.WriteLine("------------------------");
+                        weapon.Shoot();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 2:
+                        Console.WriteLine("------------------------");
+                        weapon.Fire();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 3:
+                        Console.WriteLine("------------------------");
+                        Console.WriteLine($"You need {weapon.GetRemainBulletCount()} bullets for reload");
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 4:
+                        Console.WriteLine("------------------------");
+                        weapon.Reload();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 5:
+                        Console.WriteLine("------------------------");
+                        weapon.ChangeFireMode();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 7:
+                        Console.WriteLine("Choose the command: (Q : exit)");
+                        char editChoice;
+                        do
+                        {
+                            editChoice = Convert.ToChar(Console.ReadLine().ToUpper());
+                            switch (editChoice)
+                            {
+                                case 'T':
+                                    int changedBulletCapacity;
+                                    do
+                                    {
+                                        Console.WriteLine("Enter the maximum bullet capacity:");
+                                        changedBulletCapacity = Convert.ToInt32(Console.ReadLine());
+
+                                    } while (changedBulletCapacity <= 0 || changedBulletCapacity > 120);
+                                    weapon.ChangeBulletCapacity(changedBulletCapacity);
+                                    break;
+                                case 'S':
+                                    Console.WriteLine("gulle sayi");
+                                    break;
+                                case 'D':
+                                    Console.WriteLine("saniye");
+                                    break;
+                            }
+                        } while (editChoice != 'Q');
+                        break;
+                    default:
+                        break;
+                }
+            } while (choice != 6);
+        }
+    }
+}
